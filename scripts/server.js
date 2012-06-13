4d664ba88a36504836c3dbc15b499cc7210e621e
James Lal <james@lightsofapollo.com>
2012-06-13 12:37:52 -0700

    mutli app serve

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,13 @@ var Server = require('../lib/script')({
   usage: 'server --gaia $GAIA_DIR',
 
   options: {
+
+    dir: {
+      alias: 'd',
+      desc: 'App directories may be passed multiple times relative to gaia root',
+      default: ['apps/']
+    },
+
     gaia: {
       alias: 'g',
       desc: 'path to gaia directory',
@@ -21,25 +28,58 @@ var Server = require('../lib/script')({
   var static = require('node-static'),
       fsPath = require('path'),
       fs = require('fs'),
+      dirs = [],
       port = parseInt(argv.port.toString().replace(/[^0-9]/g, ''));
+
+  if (argv.dir) {
+    if(typeof(argv.dir) === 'string') {
+      argv.dir = [argv.dir];
+    }
+    dirs = argv.dir;
+  } else {
+    dirs = ['apps/'];
+  }
+
+
+  if(dirs.length === 0) {
+    console.error('\nERROR: You must pass --dir or --gaia to provide a list of apps to serve.\n');
+    this.help(1);
+  }
 
   if (!fsPath.existsSync(argv.gaia)) {
     console.error('Invalid path: ', argv.gaia);
     this.help(1);
   }
 
-  console.log(port);
+  var apps = {};
 
-  var apps = fs.readdirSync(fsPath.join(argv.gaia, 'apps'));
+  dirs.forEach(function(dir) {
+    var path = fsPath.join(argv.gaia, dir),
+        appList = fs.readdirSync(path);
+
+    appList.forEach(function(app) {
+      if (app.indexOf('.') === 0) {
+        return;
+      }
+      apps[app] = dir;
+    });
+  });
+
+
   var file = new(static.Server)(argv.gaia);
+  var key;
+
+  for(key in apps) {
+    console.log('SERVE: ' + key + ' from  appdir: '  + apps[key]);
+  }
 
   require('http').createServer(function (request, response) {
       request.addListener('end', function () {
         var host = request.headers.host,
             app = host.split('.')[0];
 
-          if(apps.indexOf(app) !== -1) {
-            request.url = fsPath.join('/apps', app, request.url);
+          if(apps[app]) {
+            request.url = fsPath.join('/' + apps[app], app, request.url);
             file.serve(
               request, response
             );
